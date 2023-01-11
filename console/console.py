f4e7bbbaf67476d417b91a20510554f8537cce5a
MissTipo <dorine.a.tipo@gmail.com>
2023-01-11 09:06:34 +0300

    update console

--- console/console.py
+++ console/console.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 import cmd
+import shlex
+from models.base_model import BaseModel
+from models.engine.file_storage import classes
+import models
 
 
 class HBNBCommand(cmd.Cmd):
     """This is the class definition for the command interpreter"""
     prompt = "(hbnb) "
+    #classes = {'basemodel':BaseModel}
+    #classes = ["BaseModel"]
 
     def do_quit(self, line):
         """This command quits the command interpreter"""
@@ -34,7 +40,29 @@ class HBNBCommand(cmd.Cmd):
             return False
         if args[0] not in classes:
             print("class does not exist!")
-            
+            return False
+        instance = classes[args[0]]()
+        print (instance.id)
+        instance.save()
+
+    def do_show(self, line):
+        """show instance of class and uuid"""
+        args = shlex.split(line)
+        if len(args) == 0:
+            print("Class name missing")
+            return False
+        if args[0] not in classes:
+            print("class does not exist!")
+            return False
+        if len(args) == 1:
+            print("** instance id missing **")
+            return False
+        model = args[0] + "." + args[1]
+        models.storage.all()
+        if model not in models.storage.all():
+            print("** no instance found **")
+            return False
+        print(models.storage.all()[model])
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
